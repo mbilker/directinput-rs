fd71c205d681492f27473a883704c4c715eee466
Matt Bilker <me@mbilker.us>
2020-09-29 08:29:08 +0000

    cargo: fix build with MinGW

--- build.rs
+++ build.rs
@@ -1,6 +1,8 @@
 fn main() {
-    // `winapi` handles linking to `dinput8`, but `dxguid` is needed for many
-    // of the included GUID definitions. MSVC reports undefined external
-    // symbols while linking without this.
+    // `winapi`'s link definition seems enough with MSVC, but not with MinGW.
+    println!("cargo:rustc-link-lib=dylib=dinput8");
+
+    // `dxguid` is needed for many of the included GUID definitions. MSVC
+    // reports undefined external symbols while linking without this.
     println!("cargo:rustc-link-lib=dylib=dxguid");
 }
